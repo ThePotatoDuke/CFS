321ca1b88ef23412c368edfd8fc4e2c99a73cae0
Mustafa <gameacedamy@gmail.com>
2024-05-19 15:03:08 +0300

    disabled screen rotation and back button. u can fix supabase stuff with join for all i care :(

--- app/src/main/java/com/example/cfs/MainActivity.kt
+++ app/src/main/java/com/example/cfs/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.cfs
 
+import android.content.pm.ActivityInfo
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import com.example.cfs.ui.LoginViewModel
@@ -10,14 +12,27 @@ import com.example.compose.CFSTheme
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var backPressedCallback: OnBackPressedCallback
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                //do nothing
+            }
+        }
+        // Add the callback to the onBackPressedDispatcher
+        onBackPressedDispatcher.addCallback(this, backPressedCallback)
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContent {
             CFSTheme {
                 CFSApp(LoginViewModel())
             }
         }
     }
+
+
 }
 
